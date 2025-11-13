94c7ffd6739d7b725fe1e298c5eaa032fcaf590d
seria <seria.ati@gmail.com>
2025-11-13 13:22:09 +0900

    feat: Add image hash detection

--- run.py
+++ run.py
@@ -2,17 +2,21 @@ from collections import defaultdict
 import contextlib
 from dataclasses import dataclass
 import datetime
+import io
 import logging
 import os
 import re
-from typing import Any
+from typing import Any, Self
 from discord.ext import commands
 import discord
 from dotenv import load_dotenv
+from PIL import Image
+import imagehash
 
 MAX_MESSAGE_NUM = 3
 TIMEOUT_MINUTES = 15
 SPECIAL_GUILD_CHANNELS = {875392637299990628: 973232047193751582}
+IMAGE_EXTENSIONS = {".png", ".jpg", ".jpeg", ".gif", ".bmp", ".webp"}
 
 logger = logging.getLogger("discord.bot")
 
@@ -27,12 +31,53 @@ permissions = discord.Permissions(
 allowed_mentions = discord.AllowedMentions(everyone=False, roles=False, users=True)
 
 
+def get_image_hash(fp: io.BytesIO) -> imagehash.ImageHash:
+    return imagehash.average_hash(Image.open(fp))
+
+
+def images_identical(path1, path2):
+    hash1 = get_image_hash(path1)
+    hash2 = get_image_hash(path2)
+    return hash1 == hash2
+
+
 @dataclass(kw_only=True)
 class Message:
     id: int
     channel_id: int
     content: str
-    attachments: list[discord.Attachment]
+    image_hashes: frozenset[imagehash.ImageHash]
+
+    @staticmethod
+    async def extract_image_hash(image_hashes, attachment):
+        buffer = io.BytesIO()
+        image_data = await attachment.read()
+        buffer.write(image_data)
+        buffer.seek(0)
+        image_hashes.append(get_image_hash(buffer))
+
+    @classmethod
+    async def from_discord_message(cls, message: discord.Message) -> Self:
+        image_hashes = []
+
+        for attachment in message.attachments:
+            if attachment.content_type is None:
+                continue
+            if not attachment.content_type.startswith("image/"):
+                continue
+
+            filename = attachment.filename.lower()
+            if not any(filename.endswith(ext) for ext in IMAGE_EXTENSIONS):
+                continue
+
+            await cls.extract_image_hash(image_hashes, attachment)
+
+        return cls(
+            id=message.id,
+            channel_id=message.channel.id,
+            content=message.content,
+            image_hashes=frozenset(image_hashes),
+        )
 
 
 class MessageStore:
@@ -48,6 +93,8 @@ class MessageStore:
     def all_same(lst: list[Any]) -> bool:
         if not lst or len(lst) == 1:
             return False
+        if not lst[0]:
+            return False
         return all(x == lst[0] for x in lst)
 
     @staticmethod
@@ -56,17 +103,12 @@ class MessageStore:
             return True
         return len(set(lst)) == len(lst)
 
-    def add_message(self, message: discord.Message) -> None:
+    async def add_message(self, message: discord.Message) -> None:
         if message.guild is None:
             return
 
         self._messages[message.guild.id, message.author.id].append(
-            Message(
-                id=message.id,
-                channel_id=message.channel.id,
-                content=message.content,
-                attachments=message.attachments,
-            )
+            await Message.from_discord_message(message)
         )
         self.remove_message(message.guild.id, message.author.id)
 
@@ -87,18 +129,18 @@ class MessageStore:
         if len(messages) < MAX_MESSAGE_NUM:
             return False
 
-        return (
-            # All messages have the same content
-            self.all_same([message.content for message in messages])
-            # All messages are in different channels
-            and self.all_different([message.channel_id for message in messages])
-            and (
-                # All messages contain a URL or all messages have the same attachment filenames
-                all(self.contains_url(message.content) for message in messages)
-                or self.all_same(
-                    [{a.filename for a in message.attachments} for message in messages]
-                )
+        same_content = self.all_same([msg.content for msg in messages])
+        different_channels = self.all_different([msg.channel_id for msg in messages])
+        all_contain_url = all(self.contains_url(msg.content) for msg in messages)
+        same_images = self.all_same([msg.image_hashes for msg in messages])
+
+        if os.getenv("DEBUG"):
+            logger.info(
+                f"{same_content=}, {different_channels=}, {all_contain_url=}, {same_images=}"
             )
+
+        return different_channels and (
+            (all_contain_url and same_content) or (same_images)
         )
 
 
@@ -145,7 +187,7 @@ async def on_message(message: discord.Message) -> None:
         return
 
     store = bot.store
-    store.add_message(message)
+    await store.add_message(message)
 
     if store.is_scam(message):
         # Delete the scam messages
@@ -162,7 +204,7 @@ async def on_message(message: discord.Message) -> None:
                     reason="Sending scam messages",
                 )
             except (discord.NotFound, discord.Forbidden):
-                logger.info(f"Failed to time out {message.author!r}")
+                logger.info(f"Failed to timeout {message.author!r}")
                 pass
             else:
                 logger.info(f"Timed out {message.author!r}")
